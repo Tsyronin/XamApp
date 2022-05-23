08f7c6f8a7e50ea9ad8cb2ef2684e1df0bc47b0f
Pavlo <pavelcrn11@gmail.com>
2022-05-23 23:41:10 +0300

    Adds tabbed page.

--- XamApp/XamApp/ViewModels/LoginViewModel.cs
+++ XamApp/XamApp/ViewModels/LoginViewModel.cs
@@ -49,7 +49,7 @@ namespace XamApp.ViewModels
                 Settings.Email = Email;
                 Settings.Password = Password;
                 // Prefixing with `//` switches to a different navigation stack instead of pushing to the active one
-                await Shell.Current.GoToAsync($"//{nameof(AboutPage)}");
+                await Shell.Current.GoToAsync($"//{nameof(MainMenuPage)}");
             }
             else
             {
